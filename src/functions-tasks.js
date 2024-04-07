b398730a354fcd5f4671f437a7e8071eee148f89
adamanta1 <elena.brashkina2020@gmail.com>
2024-04-07 05:31:55 +0400

    feat: solve 7th task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -143,8 +143,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let attempt = 0;
+    while (attempt < attempts) {
+      try {
+        attempt = func();
+      } catch {
+        attempt += 1;
+      }
+    }
+    return attempt;
+  };
 }
 
 /**
